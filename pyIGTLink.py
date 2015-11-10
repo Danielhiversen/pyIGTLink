e30002995d70d67fb043aa30da90b32059d33988
Daniel Hoyer Iversen <Daniel.Hoyer.Iversen@sintef.no>
2015-11-10 14:16:11 +0100

    fix style

--- pyIGTLink.py
+++ pyIGTLink.py
@@ -5,6 +5,8 @@ Created on Tue Nov  3 19:17:05 2015
 @author: Daniel Hoyer Iversen
 """
 
+# pylint: disable=invalid-name
+
 import crcmod
 import numpy as np
 import signal
@@ -35,24 +37,23 @@ class PyIGTLink(SocketServer.TCPServer):
                 host = socket.gethostbyname(socket.gethostname())
             elif sys.platform.startswith('linux'):
                 import fcntl
-                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 try:
                     ifname = 'eth0'
-                    host = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
+                    host = socket.inet_ntoa(fcntl.ioctl(soc.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
                     # http://code.activestate.com/recipes/439094-get-the-ip-address-associated-with-a-network-inter/
                 except:
                     ifname = 'lo'
-                    host = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
+                    host = socket.inet_ntoa(fcntl.ioctl(soc.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
 
         SocketServer.TCPServer.allow_reuse_address = True
         SocketServer.TCPServer.__init__(self, (host, port), TCPRequestHandler)
 
-        self._message_queue = collections.deque(maxlen=buffer_size)
-        self._lock_message_queue = threading.Lock()
+        self.message_queue = collections.deque(maxlen=buffer_size)
+        self.lock_server_thread = threading.Lock()
 
         self._connected = False
-        self._shuttingDown = False
-        self._lock_connected_shuttingDown = threading.Lock()
+        self.shuttingdown = False
 
         signal.signal(signal.SIGTERM, self._SignalHandler)
         signal.signal(signal.SIGINT, self._SignalHandler)
@@ -80,21 +81,21 @@ class PyIGTLink(SocketServer.TCPServer):
             return False
 
         if self._connected:
-            with self._lock_message_queue:
-                self._message_queue.append(message)  # copy.deepcopy(message))
-            while wait and len(self._message_queue) > 0:
+            with self.lock_server_thread:
+                self.message_queue.append(message)  # copy.deepcopy(message))
+            while wait and len(self.message_queue) > 0:
                 time.sleep(0.001)
                 return True
         else:
-            if len(self._message_queue) > 0:
-                with self._lock_message_queue:
-                    self._message_queue = collections.clear()
+            if len(self.message_queue) > 0:
+                with self.lock_server_thread:
+                    self.message_queue = collections.clear()
             return False
 
     def _SignalHandler(self, signum, stackframe):
         if signum == signal.SIGTERM or signum == signal.SIGINT:
-            with self._lock_connected_shuttingDown:
-                self._shuttingDown = True
+            with self.lock_server_thread:
+                self.shuttingdown = True
             self.CloseConnection()
             _Print('YOU KILLED ME, BUT I CLOSED THE SERVER BEFORE I DIED')
             sys.exit(signum)
@@ -102,25 +103,28 @@ class PyIGTLink(SocketServer.TCPServer):
     def isConnected(self):
         return self._connected
 
+    def updateConnectedStatus(self, val):
+        self._connected = val
+
     def CloseConnection(self):
         """Will close connection and shutdown server"""
         self._connected = False
-        with self._lock_connected_shuttingDown:
-            self._shuttingDown = True
+        with self.lock_server_thread:
+            self.shuttingdown = True
         self.shutdown()
         _Print("\nServer closed\n")
 
     def _PrintIpAdressAndPortNo(self):
         while True:
             while not self._connected:
-                with self._lock_connected_shuttingDown:
-                    if self._shuttingDown:
+                with self.lock_server_thread:
+                    if self.shuttingdown:
                         break
                 _Print("No connections\nIp adress: " + str(self.GetIpAdress()) + "\nPort number: " + str(self.GetPortNo()))
                 time.sleep(5)
             time.sleep(10)
-            with self._lock_connected_shuttingDown:
-                if self._shuttingDown:
+            with self.lock_server_thread:
+                if self.shuttingdown:
                     break
 
 
@@ -129,23 +133,23 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
     Help class for PyIGTLink
     """
     def handle(self):
-        self.server._connected = True
+        self.server.updateConnectedStatus(True)
         while True:
-            if len(self.server._message_queue) > 0:
-                with self.server._lock_message_queue:
-                    message = self.server._message_queue.popleft()
+            if len(self.server.message_queue) > 0:
+                with self.server.lock_server_thread:
+                    message = self.server.message_queue.popleft()
                     response_data = message.getBinaryMessage()
                     # print "Send: " + str(message._timestamp)
                     try:
                         self.request.sendall(response_data)
                     except Exception as e:
-                        self.server._connected = False
+                        self.server.updateConnectedStatus(False)
                         _Print('ERROR, FAILED TO SEND DATA. \n'+str(e))
                         return
             else:
                 time.sleep(1/1000.0)
-                with self.server._lock_connected_shuttingDown:
-                    if self.server._shuttingDown:
+                with self.server.lock_server_thread:
+                    if self.server.shuttingdown:
                         break
 
 
@@ -176,7 +180,7 @@ class MessageBase(object):
         self._timestamp = int(time.time())
 
         self._endian = ">"  # big-endian
-        
+
         self._binary_body = None
         self._binary_head = None
         self._bodyPackSize = 0
@@ -194,7 +198,7 @@ class MessageBase(object):
         binaryMessage = binaryMessage + struct.pack(self._endian+"Q", body_size)
         binaryMessage = binaryMessage + struct.pack(self._endian+"Q", crc)
 
-        self._binary_head = binaryMessage 
+        self._binary_head = binaryMessage
 
     def getBinaryMessage(self):
         if not self._binary_head:
@@ -204,15 +208,13 @@ class MessageBase(object):
     def getBinaryBody(self):
         if not self._binary_body:
             self.PackBody()
-        return self._binary_body            
+        return self._binary_body
 
     def PackBody(self):
         self._binary_body = b""
 
     def IsValid(self):
         return self._validMessage
-                
-
 
     def GetBodyPackSize(self):
         return self._bodyPackSize
@@ -224,6 +226,11 @@ class ImageMessage(MessageBase):
         MessageBase.__init__(self)
         self._validMessage = True
         self._name = "IMAGE"
+
+        if len(image.shape) < 2:
+            self._validMessage = False
+            return
+
         try:
             self._data = np.asarray(image)
         except Exception as e:
@@ -264,12 +271,12 @@ class ImageMessage(MessageBase):
 
         self._spacing = spacing
         self._matrix = np.identity(4)  # A matrix representing the origin and the orientation of the image.
-        
+
 
     def PackBody(self):
         binaryMessage = struct.pack(self._endian+"H", IGTL_IMAGE_HEADER_VERSION)
-        
-        binaryMessage = binaryMessage + struct.pack(self._endian+"B", 1)  # Number of Image Components (1:Scalar, >1:Vector). (NOTE: Vector data is stored fully interleaved.)
+        # Number of Image Components (1:Scalar, >1:Vector). (NOTE: Vector data is stored fully interleaved.)
+        binaryMessage = binaryMessage + struct.pack(self._endian+"B", len(self._data.shape) - 1)
         binaryMessage = binaryMessage + struct.pack(self._endian+"B", self._datatype_s)
 
         if self._data.dtype.byteorder == "<":
@@ -323,6 +330,7 @@ class ImageMessage(MessageBase):
         else:
             binaryMessage = binaryMessage + struct.pack(self._endian+"H", 1)
 
+
         binaryMessage = binaryMessage + self._data.tostring(byteorder)  # struct.pack(fmt,*data)
         self._bodyPackSize = len(binaryMessage)
 
@@ -338,6 +346,7 @@ if __name__ == "__main__":
     Run as local server sending random tissue data
 
     """
+
     if False:
         IGTL_HEADER_SIZE = 58
         msg = MessageBase()
@@ -359,13 +368,14 @@ if __name__ == "__main__":
         k = 0
 
         while True:
-            if server.isConnected:
+            if server.isConnected():
                 k = k+1
                 print k
                 data = np.random.randn(samples, beams)*50+100
-                message = ImageMessage(data)
-                server.AddMessageToSendQueue(message)
-            time.sleep(1.5)
+                # data[:, :, 1] = data[:, :, 1] + 90
+                imageMessage = ImageMessage(data)
+                server.AddMessageToSendQueue(imageMessage)
+            time.sleep(0.1)
 
     elif len(sys.argv) == 2:
         print "\n\n   Run as server, sending moving circle \n\n  "
@@ -387,6 +397,8 @@ if __name__ == "__main__":
                 data = np.ones((n, n))
                 data[mask] = 255
 
-                message = ImageMessage(data)
-                server.AddMessageToSendQueue(message)
+                print data.shape
+
+                imageMessage = ImageMessage(data)
+                server.AddMessageToSendQueue(imageMessage)
             time.sleep(0.1)
