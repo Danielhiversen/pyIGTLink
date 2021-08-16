94f9b9075c63ff0ecc37d844635a65b4522239da
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-08-16 19:44:07 +0200

    0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='pyIGTLink',
-   version='0.2.1',
+   version='0.2.2',
    author='Daniel Hjelseth Høyer',
    url='https://github.com/Danielhiversen/pyIGTLink',
    description='python interface for OpenIGTLink',
